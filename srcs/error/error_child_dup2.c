479e00637a23e16661a72444e4127400830c37ad
Erwann Cord'Homme <erwannc@macbook-pro-de-erwann.home>
2021-11-09 04:23:13 +0100

    fix error but leaks ?

--- srcs/error/error_child_dup2.c
+++ srcs/error/error_child_dup2.c
@@ -6,7 +6,7 @@
 /*   By: ercordho <ercordho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 22:38:46 by ercordho          #+#    #+#             */
-/*   Updated: 2021/11/09 04:15:20 by ercordho         ###   ########.fr       */
+/*   Updated: 2021/11/09 04:23:05 by ercordho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	error_child_dup2(t_cmd *cmd, int process_index)
 	i = -1;
 	while (cmd->cmds[i])
 		ft_memdels((void **)&cmd->cmds[i], (void **)cmd->cmds[i]);
-	process_index++;
 	ft_putstr(RED);
 	if (process_index == 11)
 		ft_putendl("ERROR\nChild 1 dup2 STDOUT_FILENO failled.");
